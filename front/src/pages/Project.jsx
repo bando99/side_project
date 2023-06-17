65bae81f5724b4d49b68f7284443ad604b3090f4
GyunHee <ef3232@naver.com>
2023-06-17 15:01:28 +0900

    Design: project page & Feat: header navigate

--- front/src/pages/Project.jsx
+++ front/src/pages/Project.jsx
@@ -1,5 +1,56 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const tagMenu = styled.div`
+  border: 2px solid #dae9fc;
+  border-radius: 15px;
+  width: 7rem;
+  height: 2.5rem;
+  background-color: black;
+`;
+
+// const MainImageWrapper = styled.div`
+//   height: 500px;
+//   width: 100%;
+//   position: relative;
+//   background-image: linear-gradient(
+//       to bottom,
+//       rgba(0, 0, 0, 0) 39%,
+//       rgba(0, 0, 0, 0) 41%,
+//       rgba(0, 0, 0, 0.65) 100%
+//     ),
+//     url(${(props) => props.image});
+//   background-size: 100%, cover;
+//   background-position: center, center;
+//   background-blend-mode: overlay;
+
+//   & h2 {
+//     color: white;
+//   }
+
+//   & p {
+//     color: white;
+//     font-size: 1rem;
+//   }
+
+//   & > div {
+//     position: absolute;
+//     max-width: 500px;
+//     bottom: 2rem;
+//     margin-left: 2rem;
+//   }
+// `;
+
+const tags = styled.div``;
 
 export default function Project() {
-  return <div>projects</div>;
+  return (
+    <section>
+      <tagMenu>
+        <div>
+          <input type="text" placeholder="제목, 게시글 검색" />
+        </div>
+      </tagMenu>
+    </section>
+  );
 }
