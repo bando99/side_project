d198b814f6b84a6db6e9a3a4cae42f86c80300cc
GyunHee <ef3232@naver.com>
2023-06-24 17:14:30 +0900

    Design Project page navbar

--- front/src/pages/Project/Project.jsx
+++ front/src/pages/Project/Project.jsx
@@ -16,19 +16,33 @@ export default function Project() {
         </div>
         <div className={styles.tag__container}>
           <div className={styles.tagIcon}></div>
-          <Tag skill="JavaScript" />
-          <Tag skill="TypeScript" />
-          <Tag skill="React" />
-          <Tag skill="Vue" />
-          <Tag skill="Svelte" />
-          <Tag skill="Next.js" />
-          <Tag skill="Node.js" />
-          <Tag skill="Java" />
-          <Tag skill="Go" />
-          <Tag skill="Spring" />
-          <Tag skill="Nest.js" />
-          <Tag skill="Pigma" />
-          <Tag skill="XD" />
+          <div className={styles.tag__box}>
+            <Tag skill="JavaScript" />
+            <Tag skill="TypeScript" />
+            <Tag skill="React" />
+            <Tag skill="Vue" />
+            <Tag skill="Svelte" />
+            <Tag skill="Next.js" />
+            <Tag skill="Node.js" />
+            <Tag skill="Java" />
+            <Tag skill="Go" />
+            <Tag skill="Spring" />
+            <Tag skill="Nest.js" />
+            <Tag skill="Pigma" />
+            <Tag skill="XD" />
+          </div>
+        </div>
+        <div className={styles.part}>
+          <div className={styles.partIcon}></div>
+          <div className={styles.partList}>
+            <p className={styles.part__content}>전체</p>
+            <p className={styles.part__content}>PM</p>
+            <p className={styles.part__content}>디자이너</p>
+            <p className={styles.part__content}>프론트</p>
+            <p className={styles.part__content}>백엔드</p>
+            <p className={styles.part__content}>모바일</p>
+            <p className={styles.part__content}>기타</p>
+          </div>
         </div>
       </div>
     </section>
