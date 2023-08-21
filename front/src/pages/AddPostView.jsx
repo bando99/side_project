b0fb82d8caca856756a5067d75af48d41bc7b40b
GyunHee <ef3232@naver.com>
2023-08-21 22:53:09 +0900

    Fix: merge error (AddPost, MyPage)

--- front/src/pages/AddPostView.jsx
+++ front/src/pages/AddPostView.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import styles from './AddPost.module.css';
 import axios from 'axios';
+import styled from 'styled-components';
 
-export default function AddPostView() {
+export default function AddPost() {
   const [title, setTitle] = useState('');
   const [skill, setSkill] = useState('');
   const [type, setType] = useState('');
@@ -132,24 +133,28 @@ export default function AddPostView() {
   };
 
   return (
-    <section>
-      <div>
+    <Section>
+      <Recruitment>
         <span>모집구분</span>
-        <button
-          className={projectBtn ? styles.btnActive : styles.btn}
-          onClick={handleProejectBtn}
-        >
-          프로젝트
-        </button>
-        <button
-          className={studyBtn ? styles.btnActive : styles.btn}
-          onClick={handleStudyBtn}
-        >
-          스터디
-        </button>
-      </div>
-      <form onSubmit={handleSubmit}>
         <div>
+          <button
+            style={{ borderRadius: ' 8px 0px 0px 8px' }}
+            className={projectBtn ? 'btnActive' : 'btn'}
+            onClick={handleProejectBtn}
+          >
+            프로젝트
+          </button>
+          <button
+            style={{ borderRadius: ' 0px 8px 8px 0px' }}
+            className={studyBtn ? 'btnActive' : 'btn'}
+            onClick={handleStudyBtn}
+          >
+            스터디
+          </button>
+        </div>
+      </Recruitment>
+      <form onSubmit={handleSubmit}>
+        <Title>
           <span>글 제목</span>
           <input
             placeholder="내용을 입력해 주세요."
@@ -157,17 +162,17 @@ export default function AddPostView() {
             value={title}
             onChange={handleTitle}
           />
-        </div>
-        <div>
+        </Title>
+        <Date>
           <span>날짜 설정</span>
+          <input type="date" />
           <span>시작 날짜</span>
+          <input type="date" />
           <span>마감 날짜</span>
-        </div>
-        <div>
+          <input type="date" />
+        </Date>
+        <Skill>
           <span>사용 기술</span>
-          {tagNames.map((tagName, index) => (
-            <div key={index}>{tagName}</div>
-          ))}
           <select name="stack" id="" value={skill} onChange={handleSkill}>
             <option value="">선택</option>
             <option value="react">react</option>
@@ -176,8 +181,13 @@ export default function AddPostView() {
             <option value="flutter">flutter</option>
           </select>
           <div onClick={handleAddTags} className={styles.plusBtn}></div>
-        </div>
-        <div>
+          <div className="tags">
+            {tagNames.map((tagName, index) => (
+              <div key={index}>{tagName}</div>
+            ))}
+          </div>
+        </Skill>
+        <Playing>
           <span>진행 방식</span>
           <div>
             <input
@@ -188,98 +198,86 @@ export default function AddPostView() {
               value={proceed_method}
               onChange={handleProceedMethod}
             />
-            <button>입력</button>
           </div>
-        </div>
-        <div>
+          <button>입력</button>
+        </Playing>
+        <People>
           <span>인원 모집</span>
           <div>
-            <span>PM</span>
-            <select
-              name="pm"
-              id=""
-              value={pmCnt}
-              onChange={(e) => setPmCnt(e.target.value)}
-            >
-              <option value="">선택</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4명이상</option>
-            </select>
+            <div className="people_section">
+              <div className="people_title">PM</div>
+              <select
+                name="pm"
+                id=""
+                value={pmCnt}
+                onChange={(e) => setPmCnt(e.target.value)}
+              >
+                <option value="">선택</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4명이상</option>
+              </select>
+            </div>
+            <div className="people_section">
+              <div className="people_title">디자이너</div>
+              <select
+                name="designer"
+                value={designerCnt}
+                onChange={(e) => setDesignerCnt(e.target.value)}
+              >
+                <option value="">선택</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4명이상</option>
+              </select>
+            </div>
+            <div className="people_section">
+              <div className="people_title">프론트엔드</div>
+              <select
+                name="frontEnd"
+                value={frontEndCnt}
+                onChange={(e) => setFrontEndCnt(e.target.value)}
+              >
+                <option value="">선택</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4명이상</option>
+              </select>
+            </div>
+            <div className="people_section">
+              <div className="people_title">백엔드</div>
+              <select
+                name="backEnd"
+                value={backEndCnt}
+                onChange={(e) => setBackEndCnt(e.target.value)}
+              >
+                <option value="">선택</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4명이상</option>
+              </select>
+            </div>
+            <div className="people_section">
+              <div className="people_title">기타</div>
+              <select
+                name="etc"
+                value={etcCnt}
+                onChange={(e) => setEtcCnt(e.target.value)}
+              >
+                <option value="">선택</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4명이상</option>
+              </select>
+            </div>
           </div>
-          <div>
-            <span>모바일</span>
-            <select
-              name="mobile"
-              value={mobileCnt}
-              onChange={(e) => setMobileCnt(e.target.value)}
-            >
-              <option value="">선택</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4명이상</option>
-            </select>
-          </div>
-          <div>
-            <span>디자이너</span>
-            <select
-              name="designer"
-              value={designerCnt}
-              onChange={(e) => setDesignerCnt(e.target.value)}
-            >
-              <option value="">선택</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4명이상</option>
-            </select>
-          </div>
-          <div>
-            <span>프론트엔드</span>
-            <select
-              name="frontEnd"
-              value={frontEndCnt}
-              onChange={(e) => setFrontEndCnt(e.target.value)}
-            >
-              <option value="">선택</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4명이상</option>
-            </select>
-          </div>
-          <div>
-            <span>백엔드</span>
-            <select
-              name="backEnd"
-              value={backEndCnt}
-              onChange={(e) => setBackEndCnt(e.target.value)}
-            >
-              <option value="">선택</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4명이상</option>
-            </select>
-          </div>
-          <div>
-            <span>기타</span>
-            <select
-              name="etc"
-              value={etcCnt}
-              onChange={(e) => setEtcCnt(e.target.value)}
-            >
-              <option value="">선택</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4명이상</option>
-            </select>
-          </div>
-        </div>
-        <div>
+        </People>
+        <TextArea>
           <span>글 내용</span>
           <textarea
             name="content"
@@ -290,12 +288,232 @@ export default function AddPostView() {
             value={text}
             onChange={handleText}
           ></textarea>
-        </div>
-        <div>
+        </TextArea>
+        <Submit>
           <button>수정하기</button>
-          <button className={styles.btn}>작성 완료</button>
-        </div>
+          <button>작성 완료</button>
+        </Submit>
       </form>
-    </section>
+    </Section>
   );
 }
+
+const Section = styled.section`
+  margin: auto;
+  width: 850px;
+  margin-top: 70px;
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+  span {
+    display: inline-block;
+    width: 90px;
+    margin-right: 10px;
+  }
+`;
+
+const Recruitment = styled.div`
+  display: flex;
+  align-items: center;
+
+  div {
+    width: 700px;
+  }
+
+  .btn {
+    background-color: #1f7ceb;
+    height: 42px;
+    color: white;
+    padding: 0.5rem 1rem;
+    border: 1px solid #d2e2ec;
+    width: 126px;
+    cursor: pointer;
+  }
+
+  .btnActive {
+    width: 6rem;
+    background-color: #08438c;
+    height: 42px;
+    color: white;
+    padding: 0.5rem 1rem;
+    width: 126px;
+    cursor: pointer;
+    border: 1px solid #d2e2ec;
+  }
+`;
+
+const Date = styled.div`
+  display: flex;
+  align-items: center;
+  padding-bottom: 40px;
+  input {
+    width: 130px;
+    height: 22px;
+    border-radius: 5px;
+    border: 1px solid #d2e2ec;
+    background: var(--bs-white, #fff);
+    box-shadow: 0px 0px 0px 0px #cbdafc;
+  }
+`;
+
+const Title = styled.div`
+  display: flex;
+  align-items: center;
+  padding-bottom: 40px;
+  input {
+    width: 700px;
+    height: 21px;
+    border-radius: 5px;
+    border: 1px solid #d2e2ec;
+    background: var(--bs-white, #fff);
+    box-shadow: 0px 0px 0px 0px #cbdafc;
+  }
+`;
+const Playing = styled.div`
+  display: flex;
+  align-items: center;
+  padding-bottom: 40px;
+
+  div {
+    width: 620px;
+    height: 42px;
+    border-radius: 5px;
+    border: 1px solid #d2e2ec;
+    background: var(--bs-white, #fff);
+    box-shadow: 0px 0px 0px 0px #cbdafc;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    input {
+      margin: 0;
+      padding: 5px;
+      outline: none;
+      background: none;
+      flex: 0.95;
+    }
+  }
+  button {
+    margin-left: 12px;
+    width: 88px;
+    height: 42px;
+    border-radius: 5px;
+    border: 1px solid #d2e2ec;
+    background: #fff;
+    box-shadow: 0px 0px 0px 0px #cbdafc;
+    color: #1f7ceb;
+  }
+`;
+
+const Skill = styled.div`
+  display: flex;
+  align-items: center;
+  padding-bottom: 40px;
+  select {
+    width: 160px;
+    height: 42px;
+    border-radius: 5px;
+    border: 1px solid #d2e2ec;
+    background: var(--bs-white, #fff);
+    box-shadow: 0px 0px 0px 0px #cbdafc;
+  }
+
+  .tags {
+    display: flex;
+    gap: 5px;
+    margin-left: 10px;
+  }
+`;
+
+const People = styled.div`
+  display: flex;
+  padding-bottom: 40px;
+  .people_section {
+    display: flex;
+    margin-bottom: 10px;
+    gap: 10px;
+  }
+
+  .people_title {
+    width: 150px;
+    height: 42px;
+    border-radius: 5px;
+    border: 1px solid #d2e2ec;
+    background: var(--bs-white, #fff);
+    box-shadow: 0px 0px 0px 0px #cbdafc;
+    line-height: 42px;
+    padding-left: 10px;
+  }
+
+  select {
+    width: 160px;
+    height: 42px;
+    border-radius: 5px;
+    border: 1px solid #d2e2ec;
+    background: var(--bs-white, #fff);
+    box-shadow: 0px 0px 0px 0px #cbdafc;
+  }
+`;
+
+const TextArea = styled.div`
+  display: flex;
+  padding-bottom: 40px;
+
+  textarea {
+    width: 710px;
+    border-radius: 5px;
+    border: 1px solid #d2e2ec;
+    background: var(--bs-white, #fff);
+    box-shadow: 0px 0px 0px 0px #cbdafc;
+  }
+`;
+
+const Submit = styled.div`
+  width: 100%;
+  display: flex;
+  margin: 30px auto;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+
+  button:first-child {
+    border-radius: 5px;
+    border: 2px solid #d2e2ec;
+    background: #fff;
+    box-shadow: 0px 0px 0px 0px #cbdafc;
+    display: flex;
+    width: 100px;
+    height: 42px;
+    padding: 10px;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    flex-shrink: 0;
+    color: #1f7ceb;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    cursor: pointer;
+  }
+  button:nth-child(2) {
+    border-radius: 5px;
+    border: 2px solid #d2e2ec;
+    background: #1f7ceb;
+    box-shadow: 0px 0px 0px 0px #cbdafc;
+    display: flex;
+    width: 100px;
+    height: 42px;
+    padding: 10px;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    flex-shrink: 0;
+    color: white;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    cursor: pointer;
+  }
+`;
