fb5fd6683e6001dc2431f02f408e9491842dd1fd
GyunHee <ef3232@naver.com>
2023-09-02 22:34:57 +0900

    Design: add styled-component Login page

--- front/src/pages/User/Login/LoginView.jsx
+++ front/src/pages/User/Login/LoginView.jsx
@@ -1,9 +1,143 @@
 import React, { useState } from 'react';
-import styles from './LoginView.module.css';
+import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
-export default function LoginView() {
+const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Title = styled.p`
+  font-weight: 800;
+`;
+
+const ContainerBox = styled.div`
+  border: 2px solid #d2e2ec;
+  border-radius: 20px;
+  padding: 3rem 8rem;
+`;
+
+const InputContainer = styled.div`
+  .input__text {
+    font-weight: 700;
+    margin-top: 1rem;
+    margin-bottom: 0.4rem;
+  }
+
+  input {
+    width: 100%;
+    border: 1px solid #d2e2ec;
+  }
+
+  .password__input {
+    background-color: #d2e2ec;
+    margin-bottom: 0.7rem;
+  }
+`;
+
+const LoginBtnContainer = styled.div`
+  margin-top: 2rem;
+  text-align: center;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  margin: auto;
+  width: 90%;
+`;
+
+const MenuText = styled.p`
+  font-size: 0.5rem;
+  margin: 0.2rem;
+  cursor: pointer;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const LoginBtn = styled.button`
+  margin: auto;
+  background-color: #1f7ceb;
+  width: 70%;
+  border-radius: 25px;
+  color: #ffffff;
+  padding: 0.5rem 1rem;
+  border: none;
+  margin: 1rem;
+`;
+
+const LoginSNS = styled.p`
+  font-weight: 800;
+  margin: 2rem;
+`;
+
+const SNSContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const SNSIconContainer = styled.div`
+  margin: 0 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .kakao {
+    background-image: url('/sns/kakaoImage.png');
+    background-size: cover;
+    width: 3rem;
+    height: 3rem;
+    margin: 1rem 0;
+  }
+
+  .naver {
+    background-image: url('/sns/naverImage.png');
+    background-size: cover;
+    width: 3rem;
+    height: 3rem;
+    margin: 1rem 0;
+  }
+
+  .github {
+    background-image: url('/sns/githubImage.png');
+    background-size: cover;
+    width: 3rem;
+    height: 3rem;
+    margin: 1rem 0;
+  }
+`;
+
+const SNSText = styled.span`
+  font-size: 0.8rem;
+`;
+
+const JoinContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const JoinQA = styled.p`
+  font-size: 0.8rem;
+  margin-top: 2.4rem;
+  margin-bottom: 1rem;
+  font-weight: 400;
+`;
+
+const JoinText = styled.p`
+  font-weight: 900;
+  text-decoration: underline;
+  cursor: pointer;
+
+  &:hover {
+    text-decoration: none;
+  }
+`;
+
+function LoginView() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
@@ -29,7 +163,7 @@ export default function LoginView() {
     navigate('/user/pwChange');
   };
 
-  const hadnleLogin = async (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
     console.log('로그인 정보:', {
       username,
@@ -50,64 +184,56 @@ export default function LoginView() {
   };
 
   return (
-    <section className={styles.container}>
-      <p className={styles.title}>로그인</p>
-      <div className={styles.container__box}>
-        <form onSubmit={hadnleLogin}>
-          <div className={styles.input__container}>
-            <p>아이디</p>
+    <Container>
+      <Title>로그인</Title>
+      <ContainerBox>
+        <form onSubmit={handleLogin}>
+          <InputContainer>
+            <p className="input__text">아이디</p>
             <input
               type="text"
               onChange={handleUsernameChange}
               placeholder="내용을 입력해 주세요."
             />
-            <p>비밀번호</p>
+            <p className="input__text">비밀번호</p>
             <input
+              className="password__input"
               type="password"
               onChange={handlePasswordChange}
               placeholder="내용을 입력해 주세요."
             />
-            <div className={styles.loginBtn__container}>
-              <button className={styles.loginBtn}>로그인</button>
-            </div>
-          </div>
+            <LoginBtnContainer>
+              <LoginBtn>로그인</LoginBtn>
+            </LoginBtnContainer>
+          </InputContainer>
         </form>
-        <div className={styles.button__container}>
-          <div className={styles.modify__container}>
-            <p className={styles.menu__text} onClick={handleIdFound}>
-              아이디 찾기
-            </p>
-            <p className={styles.menu__text} onClick={handlePwFound}>
-              비밀번호 찾기
-            </p>
-            <p className={styles.menu__text} onClick={handlePwChange}>
-              비밀번호 변경
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <p className={styles.loginSNS}>SNS로 로그인 하기</p>
-      <div className={styles.sns__container}>
-        <div className={styles.snsIcon__container}>
-          <div className={styles.kakao}></div>
-          <span className={styles.SNStext}>Kakao로 로그인 하기</span>
-        </div>
-        <div className={styles.snsIcon__container}>
-          <div className={styles.naver}></div>
-          <span className={styles.SNStext}>Naver로 로그인 하기</span>
-        </div>
-        <div className={styles.snsIcon__container}>
-          <div className={styles.github}></div>
-          <span className={styles.SNStext}>Github로 로그인 하기</span>
-        </div>
-      </div>
-      <div className={styles.join__container}>
-        <p className={styles.join__qa}>아직 회원이 아니세요?</p>
-        <p className={styles.join__text} onClick={() => navigate('/join')}>
-          회원가입하기
-        </p>
-      </div>
-    </section>
+        <ButtonContainer>
+          <MenuText onClick={handleIdFound}>아이디 찾기</MenuText>
+          <MenuText onClick={handlePwFound}>비밀번호 찾기</MenuText>
+          <MenuText onClick={handlePwChange}>비밀번호 변경</MenuText>
+        </ButtonContainer>
+      </ContainerBox>
+      <LoginSNS>SNS로 로그인 하기</LoginSNS>
+      <SNSContainer>
+        <SNSIconContainer>
+          <div className="kakao"></div>
+          <SNSText>Kakao로 로그인 하기</SNSText>
+        </SNSIconContainer>
+        <SNSIconContainer>
+          <div className="naver"></div>
+          <SNSText>Naver로 로그인 하기</SNSText>
+        </SNSIconContainer>
+        <SNSIconContainer>
+          <div className="github"></div>
+          <SNSText>Github로 로그인 하기</SNSText>
+        </SNSIconContainer>
+      </SNSContainer>
+      <JoinContainer>
+        <JoinQA>아직 회원이 아니세요?</JoinQA>
+        <JoinText onClick={() => navigate('/join')}>회원가입하기</JoinText>
+      </JoinContainer>
+    </Container>
   );
 }
+
+export default LoginView;
