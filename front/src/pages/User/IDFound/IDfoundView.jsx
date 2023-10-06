1387205f3300ccae9c08669c3d4fccff74011dcb
GyunHee <ef3232@naver.com>
2023-10-06 23:02:54 +0900

    Feat: add findId API in IDFoundView

--- front/src/pages/User/IDFound/IDfoundView.jsx
+++ front/src/pages/User/IDFound/IDfoundView.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './IDfoundView.module.css';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 export default function IDfoundView() {
   const navigate = useNavigate();
+
+  const [mail, setMail] = useState('');
+  const [notMail, setNotMail] = useState(null);
 
   const handlePW = () => {
     navigate('/user/pwFound');
@@ -13,23 +17,61 @@ export default function IDfoundView() {
     navigate('/user/login');
   };
 
+  const handleChangeMail = (e) => {
+    setMail(e.target.value);
+  };
+
+  const findId = async () => {
+    const mailInfo = {
+      mail,
+    };
+
+    try {
+      const response = await axios.post(
+        'http://1.246.104.170:8080/find/findId',
+        mailInfo
+      );
+      console.log(response.message);
+    } catch (error) {
+      console.error('아이디 찾기 실패', error);
+      console.log(error.response.data.message);
+      if (
+        error.response.data.message ===
+        `Sign Exception. ${mail}은 없는 mail정보입니다.`
+      ) {
+        console.log('들어옴');
+        setNotMail(`${mail}은 없는 mail정보입니다.`);
+        console.log(notMail);
+      }
+    }
+  };
+
   return (
     <section className={styles.container}>
       <p className={styles.title}>아이디 찾기</p>
       <div className={styles.container__box}>
         <p className={styles.email}>이메일 주소</p>
         <div className={styles.input__box}>
-          <input type="text" placeholder="내용을 입력해 주세요." />
-          <button className={styles.check__btn}>인증 요청</button>
+          <input
+            type="text"
+            value={mail}
+            onChange={handleChangeMail}
+            placeholder="내용을 입력해 주세요."
+          />
+          <button className={styles.check__btn} onClick={findId}>
+            인증 요청
+          </button>
         </div>
-        <div className={styles.input__box}>
+        <p>mail은 없는 mail정보입니다.</p>
+        {/* {notMail && <p>{notMail}</p>} */}
+        {/* <div className={styles.input__box}>
           <input
             className={styles.input__code}
             type="text"
             placeholder="내용을 입력해 주세요."
           />
           <button className={styles.check__btn}>인증 확인</button>
-        </div>
+        </div> */}
         <div className={styles.ID__box}>
           <p className={styles.ID__commnet}>회원님의 아이디는</p>
           <p className={styles.ID__content}>abc1234</p>
